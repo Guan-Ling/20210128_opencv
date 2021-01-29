6cdb27c1239c8d287f5492ee09e573daf82cf731
Guan Ling <x5j035j03x@gmail.com>
2021-01-29 12:01:42 +0800

    20210129練習opencv

--- test09_1405_MOUSE_perspective_transform.py
+++ test09_1405_MOUSE_perspective_transform.py
@@ -10,9 +10,11 @@ def savexy(event,x,y,flags,param):
         print(x,y)
         pts1[i]=[x,y]
         i+=1
-img=cv2.imread('test10.jpg')
+# img=cv2.imread('test10.jpg')
+img=cv2.imread('test_09_number.jpg')
 rows,cols,ch=img.shape
-pts2 = np.float32([[0,0],[300,0],[300,300],[0,300]]) #左上開始 順時針點
+# pts2 = np.float32([[0,0],[300,0],[300,300],[0,300]])
+pts2 = np.float32([[0,0],[400,0],[400,200],[0,200]]) #左上開始 順時針點
 cv2.namedWindow('image')
 cv2.setMouseCallback('image',savexy)
 while(1):
@@ -21,7 +23,8 @@ while(1):
         break
 cv2.destroyAllWindows()
 M=cv2.getPerspectiveTransform(pts1,pts2)
-dst=cv2.warpPerspective(img,M,(300,300))
+# dst=cv2.warpPerspective(img,M,(300,300))
+dst=cv2.warpPerspective(img,M,(400,200))
 dst1 = cv2.cvtColor(dst,cv2.COLOR_BGR2RGB)
 plt.subplot(211), plt.imshow(img), plt.title('Input')
 plt.subplot(212), plt.imshow(dst1), plt.title('Output')
